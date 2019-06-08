fb61f2f8278a6c3369f51c0dc2e6d80fe71b6221
sergkryk <41703448+sergkryk@users.noreply.github.com>
2019-06-08 15:02:02 +0300

    доработал range и добавил простейшие скрипты

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var link = document.querySelector(".login-field-link");
+    link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      console.log("link is pressed");
+    });
